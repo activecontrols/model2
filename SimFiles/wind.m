78346adec850a77e3195ac5ee7ea117e35d3ae87
Andrew Lullo <andrew.lullo5@gmail.com>
2025-10-15 14:59:57 -0400

    Gust Model

--- SimFiles/wind.m
+++ SimFiles/wind.m
@@ -1,14 +1,16 @@
-function final_xdot = wind(xdot, wind_vel, mass)
+function total_vect = wind(mass, wind_vel, ASTRA_vel)
+
+rel_vel = wind_vel + ASTRA_vel
 
 % Constants needed for force calculation
 AREA = 0.07; % Lateral surface area of ASTRA - m^2
 AIR_DENSITY = 1.246656; % Estimated air desnsity - kg/m^3
 % (plan to calculate more accuraately with sensor information like temperature)
 % mass = 0.9950; % ASTRA mass (couldn't figure out how to get it from constantsASTRA) - kg
-Cd = 0.9; % Estimated Drag coefficient
+Cd = 0.95; % Estimated Drag coefficient
 
 % Calculates component accelerations from drag equation and wind velocity
-wind_acel = (1/2) * wind_vel.^2 * AREA * AIR_DENSITY * Cd / mass;
+wind_acel = (1/2) * rel_vel.^2 * AREA * AIR_DENSITY * Cd / mass;
 
 % Turning South-West output of function to North-West coordinate system
 WtE = [-1, 0; 
@@ -17,6 +19,5 @@ acel_vect = transpose(wind_acel) * WtE;
 
 % Turns the acceleration into a 15 state vector to add to xdot
 total_vect = [zeros(6,1); transpose(acel_vect); zeros(7,1)];
-final_xdot = xdot + total_vect;
 
 end
