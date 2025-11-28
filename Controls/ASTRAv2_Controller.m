3671e44e4ffa9538b2111dc7a0d9eea841d83b28
pabloplataa <p.plata2110@gmail.com>
2025-11-27 21:39:17 -0600

    Current state of things

--- Controls/ASTRAv2_Controller.m
+++ Controls/ASTRAv2_Controller.m
@@ -1,8 +1,9 @@
-% Version 2 Controller formulation for ASTRAv2. Structure consists of 2
-% cascaded loops (technically three, but first loop is merged with the trajectory
-% generation software.) 
+% Version 2 Controller formulation for ASTRAv2. Structure consists of 3
+% cascaded loops.
 %
-% First loop is a PI Loop in charge of Vel -> Acceleration commands, fitted
+% First Loop is a P Loop in charge of Pos -> Vel commands.
+%
+% Second loop is a PI Loop in charge of Vel -> Acceleration commands, fitted
 % with anti-windup and soft-gatingcbased on attitude error.
 %
 % Second loop is an LQRi Loop in charge of Target Attitude -> Gimbal +
@@ -13,4 +14,72 @@
 % channels.
 %
 % By: Pablo Plata   -   11/27/25 (Happy Thanksgiving!)
+function U = ASTRAv2_Controller(PosTarget, X, constantsASTRA, t)
+
+% Time Counter
+persistent lastT VelErrorI AttErrorI
+if isempty(error_pos)
+    lastT = 0;
+    VelErrorI = zeros(3,1);
+    AttErrorI = zeros(3,1);
+end
+dT = t - lastT;
+
+%% First Loop (P Loop)
+    % Position Error Vector
+    PosError = PosTarget - X(5:7);
+    
+    % Velocity Command
+    K_P = [0.7; 0.7; 0.7];
+    VelTarget = VelGain_P .* PosError;
+
+    % ADD Velocity Saturation Step
+
+%% Second Loop (PI Loop)
+    % Velocity Error Vector
+    VelError = VelTarget - X(8:10);
+
+    % Integral Accumulator
+    K_I = [0.05; 0.05; 0.05];
+    MaxAttError = [0.2; 0.2; 0.2];
+    Leak = 0.1;
+    Clamp = [3; 3; 5];
+
+    % Soft Gating for Integral Accumulator and Clamping
+    Gate = max(min(1 - abs(AttError) ./ MaxAttError, 1), Leak);
+    K_I = K_I * Gate;
+    VelErrorI = VelErrorI + K_I .* VelError .* dT;
+    VelErrorI = max(min(VelErrorI, Clamp), -Clamp);
+    K_P = [0.1; 0.1; 0.1];
+
+    % Acceleration Target
+    AccelTarget = K_P .* VelError + K_I * VelErrorI;
+
+    % ADD Acceleration Saturation Step. Constraint to 20° Cone for laterals
+
+%% Kinematics Step
+    % Compute thrust target
+    TargetForce_I = constantsASTRA.m * AccelTarget + [0; 0; constantsASTRA.g];
+    TargetForce_B = quatRot(X(1:4)) * TargetForce_I;
+    U(3) = TargetForce_B(3);
+    
+    % Compute target attitude via GSP.
+    AccelTarget(3) = max(AccelTarget(3), constantsASTRA.g);
+    Z = AccelTarget / norm(AccelTarget);
+
+    % Heading reference (+X axis rolled to north)
+    HDGRef = [1; 0; 0];
+    Y = cross(Z, HDGRef);
+    Y = Y / norm(Y);
+
+    % Complete the triad
+    X = cross(Y, Z);
+
+    % Create DCM and convert to quaternion
+    DCM = [X Y Z];
+    TargetAtt = DCM_Quat_Conversion;
+
+
+%% Third Loop (LQRi)
+    
 
---
+++ Controls/Controller2_Gen.m
@@ -0,0 +1,93 @@
+function K = Controller2_Gem(constants)
+%% Generation script for v2 Controls (LQRi Step, Third Loop)
+clear;
+% Symbolic Variables
+syms q0 q1 q2 q3            % earth-body quaternion
+syms omega1 omega2 omega3   % earth-body angular velocity
+syms theta phi;             % thrust angles
+syms thrust                 % thrust magnitude
+syms roll                   % roll input (rad/sec^2)
+syms m l g rTB              % system constants
+syms J [3 3]                % Intertia Matrix
+
+% Load in constantsASTRA
+constants = constructConstants;
+
+%% Equations of Motion
+%Full quaternion vector
+q = [q0; q1; q2; q3];
+
+%DCM from Earth to Body
+C_BI = quatRot(q);
+
+%DCM from Body to Earth
+C_IB = C_BI.';
+
+%Angular velocity (taken wrt E-frame, represented in B-frame) 
+omegaB = [omega1; omega2; omega3];
+
+%Body frame thrust vector
+T_vec = [cos(theta)*sin(phi); -sin(theta); cos(theta)*cos(phi)];
+TB = thrust * T_vec;
+
+% Torque vector
+MB = zetaCross([0; 0; rTB])*TB;
+M = [q(1) -q(2) -q(3) -q(4);
+     q(2)  q(1) -q(4)  q(3);
+     q(3)  q(4)  q(1) -q(2);
+     q(4) -q(3)  q(2)  q(1)];
+
+% Dynamics
+qdot = 0.5 * M * [0; omegaB];
+omegaBdot = J^(-1) * (MB - zetaCross(omegaB) * J * omegaB) + (T_vec * roll);
+
+% State vectors
+x = [q; omegaB];
+xdot = [qdot; omegaBdot];
+u = [theta; phi; roll];
+
+% Substitude constants in
+constVal = [constants.m; constants.l; constants.g; constants.rTB; constants.J(:);
+            constants.m * constants.g];
+constVec = [m; l; g; rTB; J(:); thrust];
+
+% Subsitute numeric values of constants into xdot
+xdot = subs(xdot, constVec, constVal);
+
+%% Linearization and Controller Generaiton
+lin.A = jacobian(xdot, x);
+lin.B = jacobian(xdot, u);
+
+% Map to 6 states.
+T = [zeros(1,6); eye(6)];
+T(1:4,1:3) = 0.5 * [zeros(1,3); eye(3)];
+lin.A = T' * lin.A * T;
+lin.B = T' * lin.B;
+
+% Linearization point
+delx = [1; 0; 0; 0; 0; 0; 0];
+delu = [0; 0; 0];
+lin.A = double(subs(lin.A, [x; u], [delx; delu]));
+lin.B = double(subs(lin.B, [x; u], [delx; delu]));
+
+% Augment system with Error-Integral
+lin.A = [lin.A zeros(6,3);
+         eye(3) zeros(3,6)];
+lin.B = [lin.B; zeros(3)];
+
+% Hand tuning for Q for now
+a_weights = ones(6,1);
+a_weights = a_weights / norm(a_weights);
+max_x = [3, 3, 0.3, pi/8, pi/8, 0.5];
+Q = eye(6) .* a_weights ./ max_x.^2;
+R = diag([260, 260, 0.2]);
+
+% Augment Q with integral states
+Qi = diag([0.01, 0.01, 0.002]);
+Q = [Q zeros(6,3);
+     zeros(3,6) Qi];
+
+% Initial Controller Solution
+K = lqr(lin.A, lin.B, Q, R);
+end
+
